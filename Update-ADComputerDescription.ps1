f0ca847d8ac9dd8dba2ba4c42223918199faa5f2
Bradley Herbst <Bordwalk2000@gmail.com>
2018-03-15 16:01:08 -0500

    Parse Description and Creates Objects
    I have the script parse the description and assign the different sections to a property of an object.

--- Update-ADComputerDescription.ps1
+++ Update-ADComputerDescription.ps1
@@ -1,41 +1,52 @@
-﻿$OUSearchLocation = "OU=Reboot,OU=Computers,OU=mo-stl-obrien,DC=ametek,DC=com", "OU=No_Reboot,OU=Computers,OU=mo-stl-obrien,DC=ametek,DC=com"
+﻿$OUSearchLocation = "OU=Reboot,OU=Computers,OU=mo-stl-obrien,DC=ametek,DC=com" , "OU=No_Reboot,OU=Computers,OU=mo-stl-obrien,DC=ametek,DC=com"
+$ComputerObjects=@()
 
 $OUSearchLocation | % {
 
-    Get-ADComputer -filter {Enabled -eq "True"} -Properties Description -searchbase $_  | #Select -first 1 | 
+    Get-ADComputer -filter {Enabled -eq "True"} -Properties Description -searchbase $_ | 
     % {
+        $object = New-Object PSObject -Property @{
+            Name = $_.Name
+            PrimaryUser = ''
+            ServiceTag = ''
+            AssetTag = ''
+            InstallDate = ''
+        }
+        
         if($_.Description) {
             $DescriptionSplit = $_.Description.Split('|').trim()
 
-            if($DescriptionSplit.count -gt 1) {
+            $PrimaryUser = if($DescriptionSplit.count -gt 1){ $DescriptionSplit[0] } Else{ $DescriptionSplit}
+            if($PrimaryUser -notmatch "[0-9]" -or $PrimaryUser -cnotmatch “[^A-Z]”) {
+                $object.PrimaryUser = $PrimaryUser
+            }
 
-                if($DescriptionSplit[0] -match "[0-9]" -or $DescriptionSplit[0] -cmatch “^[A-Z]*$”) {
-                    $_.Name + " Name: " + $DescriptionSplit[0]
-                    
-                }
+            if($DescriptionSplit.count -gt 2) {
 
                 if(![string]::IsNullOrEmpty($DescriptionSplit[1])){
-                    if($DescriptionSplit[1] -notmatch "[0-9]" -and $DescriptionSplit[1] -cnotmatch “^[A-Z]*$” -or $DescriptionSplit[1] -clike "C00*") {
-                        $_.Name + " Dell Service Tag: " + $DescriptionSplit[1]
+                    if($DescriptionSplit[1] -match "[0-9]" -and $DescriptionSplit[1] -cmatch “[^A-Z]” -or $DescriptionSplit[1] -cnotlike "C00*") {
+                        $object.ServiceTag = $DescriptionSplit[1]
                     }
                 }
+                
 
                 if(![string]::IsNullOrEmpty($DescriptionSplit[2])){
-                    if($DescriptionSplit[2] -cnotlike "C00*" -and $DescriptionSplit[2] -notmatch "[0-9]" ) {
-                        $_.Name + " Serial Number: " + $DescriptionSplit[2]
+                    if($DescriptionSplit[2] -clike "C00*" -and $DescriptionSplit[2] -match "[0-9]" -and $DescriptionSplit[2].Length -eq 6) {
+                        $object.AssetTag = $DescriptionSplit[2]
                     }
                 }
 
                 if(![string]::IsNullOrEmpty($DescriptionSplit[3])){
-                    if($DescriptionSplit[3].Split(' ').count -ne 2) {
-                        $_.Name + " Install Date: " + $DescriptionSplit[3]
-                    } elseif (@("Deployed","Reinstalled") -notcontains $DescriptionSplit[3].Split(' ')[0] -and ![boolean][DateTime]::Parse($DescriptionSplit[3].Split(' ')[-1])){
-                        $_.Name + " Install Date: " + $DescriptionSplit[3].Split(' ')
+                    if($DescriptionSplit[3].Split(' ').count -eq 2) {
+                    
+                        if (@("Deployed","Reinstalled") -contains $DescriptionSplit[3].Split(' ')[0] -and [boolean][DateTime]::Parse($DescriptionSplit[3].Split(' ')[-1])){
+                            $object.InstallDate = $DescriptionSplit[3]
+                        }
                     }
                 }
             }
         }
-        
-        #if(Test-Connection $_.Name -Count 1 -ea SilentlyContinue) {  }
+        $ComputerObjects += $object
     }
 }
+$ComputerObjects | Sort Name | ft Name, PrimaryUser, ServiceTag, AssetTag, InstallDate
