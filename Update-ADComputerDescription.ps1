b34e56c0c69242b85dc5eaea2e607b93e92adf00
Bradley Herbst <bradley.herbst@ametek.com>
2021-08-20 15:33:38 -0500

    Added Error Handling and Verbose Logging
    Added Try Catch blocks around the code that was querying the computers.
    Verbose messages were also added for better visibility into the running
    state of the script.

--- Update-ADComputerDescription.ps1
+++ Update-ADComputerDescription.ps1
@@ -50,45 +50,66 @@ PROCESS {
     }
 }
 
-$ComputerObjects | % {
-    if(Test-Connection $_.Name -Count 2 -ea SilentlyContinue) {
-
-        if($_.Name -notlike "MO-STL-MIS*") {
-            $PrimaryUserName = (Get-WinEvent -ComputerName $_.Name -MaxEvents 300 -ErrorAction SilentlyContinue -FilterHashtable @{LogName ='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational';ID=21} |  
-            ? {$_.Properties[2].value -eq "local" -and  $_.Properties[0].value -notlike "*admin*" -and  $_.Properties[0].value -notlike "*setup*"} | 
-            Select -First 7 | 
+    $ComputerObjects | ForEach-Object {
+        if (Test-Connection $_.Name -Count 2 -ea SilentlyContinue) {
+            Write-Verbose "$($_.Name) Online - Pulling Information"
+            try {
+                if ($_.Name -notlike "*-MIS-*") {
+                    $PrimaryUserName = (Get-WinEvent -ComputerName $_.Name -MaxEvents 300 -ErrorAction Stop -FilterHashtable @{LogName ='Microsoft-Windows-TerminalServices-LocalSessionManager/Operational';ID=21} |  
+                    Where-Object {$_.Properties[2].value -eq "local" -and $_.Properties[0].value -notlike "*admin*" -and $_.Properties[0].value -notlike "*setup*"} | 
+                    Select-Object -First 15 | 
             Group-Object {$_.Properties[0].value} |
-            Select count,name,@{ Name = 'Latest'; Expression = { ($_.Group | Measure-Object -Property TimeCreated -Maximum).Maximum } } |
-            Sort Count ,Latest -Descending |
-            Select Name -first 1)
+                    Select-Object Count, Name, @{ Name = 'Latest'; Expression = { ($_.Group | Measure-Object -Property TimeCreated -Maximum).Maximum } } |
+                    Sort-Object Count, Latest -Descending |
+                    Select-Object Name -first 1)
         
-            if($PrimaryUserName) { 
+                    if ($PrimaryUserName) { 
                 $_.PrimaryUser = (Get-ADuser $PrimaryUserName.name.split('\')[-1]).Name
                 Remove-Variable PrimaryUserName -ErrorAction SilentlyContinue
             }
         }
+            } catch {
+                Write-Verbose "Unable to access Get-WinEvent"
+            }
 
-        $ServiceTag = (Get-WmiObject Win32_BIOS -Computername $_.Name).SerialNumber
-        if($ServiceTag) {
+            try {
+                $ServiceTag = (Get-WmiObject Win32_BIOS -Computername $_.Name -ErrorAction Stop).SerialNumber
+                if ($ServiceTag) {
             $_.ServiceTag = $ServiceTag
         }
+                }
+            } catch {
+                Write-Verbose "Unable to access Get-WmiObject Win32_BIOS"
+            }
 
-        $AssetTag = (Get-WmiObject Win32_SystemEnclosure -Computername $_.Name).SMBiosAssetTag
-        if($AssetTag) {
+            try {
+                $AssetTag = (Get-WmiObject Win32_SystemEnclosure -Computername $_.Name -ErrorAction Stop).SMBiosAssetTag.split(' ')[0]
+                if ($AssetTag) {
             $_.AssetTag = $AssetTag.ToUpper()
         } elseif ($_.AssetTag -and $_.AssetTag.Length -eq 6) {
             $_.AssetTag = $_.AssetTag + " ***MISSING IN BIOS***"
         }
+            } catch {
+                Write-Verbose "Unable to access Get-WmiObject Win32_SystemEnclosure"
+            }
 
-        if(!$_.InstallDate) {
-            $_.InstallDate = "Deployed " + ([WMI]"").ConvertToDateTime((Get-WmiObject Win32_OperatingSystem -ComputerName mo-stl-mfg-dt06).InstallDate).ToString("yyyy-MM-dd")
+            if (!$_.InstallDate) {
+                try {
+                    $_.InstallDate = "Deployed " + ([WMI]"").ConvertToDateTime((Get-WmiObject Win32_OperatingSystem -ComputerName $_.Name -ea Stop).InstallDate).ToString("yyyy-MM-dd")
+                } catch {
+                    Write-Verbose "Unable to access Get-WmiObject Win32_OperatingSystem"
+                }
         }
 
         $GeneratedDescription = [String]::Join(" | ", @($_.Primaryuser, $_.ServiceTag, $_.AssetTag.trim(), $_.InstallDate))
 
-        if($_.Description -ne $GeneratedDescription)
+            if ($_.Description -ne $GeneratedDescription)
         {
+                Write-Verbose "Updating $($_.Name) Description from $($_.Description) to $GeneratedDescription"
             Set-ADComputer -Identity $_.Name -Description $GeneratedDescription
+            }
+        } else {
+            Write-Verbose "$($_.Name) is Offline"
         }
     }
 }
